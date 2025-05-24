e359aa0c1c06e74356f3f17b8427abfed420ad4b
HideakiTomori <hide18tomo@icloud.com>
2025-05-24 23:53:57 +0900

    ESLint設定 Appコンポーネントに3Dシーンの初期化を実装

--- 3d-model-website/src/App.tsx
+++ 3d-model-website/src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import * as THREE from 'three';
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const canvas: HTMLCanvasElement = document.querySelector('#canvas') as HTMLCanvasElement;
+
+    // scene
+    const scene: THREE.Scene = new THREE.Scene();
+
+    // camera
+    const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.z = 5;
+    scene.add(camera);
+    
+    // renderer
+    const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+      antialias: true,
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    // object
+    const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(1, 1, 1);
+    const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube: THREE.Mesh = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    
+    // animation
+    const tick = () => {
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+
+      requestAnimationFrame(tick);
+    }
+    tick();
+
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <canvas id="canvas"></canvas>
+      <div className="mainContent">hello world</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
